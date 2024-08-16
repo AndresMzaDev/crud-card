b001448a36058924e12acffef8bd49a7c268f8fc
Andres Meza <37252424+AndresMzaDev@users.noreply.github.com>
2024-08-16 17:05:34 -0600

    fix: creacion de componente principal

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,28 @@
+/**
+ * plugins/vuetify.js
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+// Composables
+import { createVuetify } from "vuetify";
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  theme: {
+    defaultTheme: "light",
+    themes: {
+      light: {
+        colors: {
+          primary: "#43C25F",
+          secondary: "#3C3D3B",
+          background: "#DCEAEE",
+        },
+      },
+    },
+  },
+});
---
+++ src/view/HomePage.vue
@@ -0,0 +1,8 @@
+
+
+
+<template>
+    <v-container>
+        <h1>Hola</h1>
+    </v-container>
+</template>

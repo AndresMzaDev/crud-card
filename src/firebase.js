35984e993fa8d1b516347d5f59aa71e68a3cb58a
Andres Meza <37252424+AndresMzaDev@users.noreply.github.com>
2024-08-17 02:34:10 -0600

    fite: install firebase deploy automaticos

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2zvZpVMrgfpEHKcPN3nyR9J--4TYQ9X0",
+  authDomain: "crud-car-45478.firebaseapp.com",
+  projectId: "crud-car-45478",
+  storageBucket: "crud-car-45478.appspot.com",
+  messagingSenderId: "318947503293",
+  appId: "1:318947503293:web:102d26aa3ddb3b5d10dbd4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
